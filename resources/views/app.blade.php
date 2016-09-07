5bde5d63b78be358dd4b0a1a21703159b79616ec
Alex Simangunsong <alexbenys8@gmail.com>
2016-09-07 17:38:49 +1000

    added laravel files to repo

---
+++ resources/views/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Literacy</title>
+
+    {{--Bootstrap Default CSS--}}
+    <link rel="stylesheet" href="{{ asset('bootstrap-3.3.7-dist/css/bootstrap.css') }}">
+
+    {{--Additional CSS--}}
+    <link rel="stylesheet" href="{{ asset('css/master.css') }}">
+    @yield('css')
+
+    {{--Additional JS Files--}}
+    @yield('js')
+
+</head>
+<body>
+    <nav>
+
+    </nav>
+
+    @yield('content')
+
+    <footer>
+        <div class="text-center">
+            <a href="#">Source Log</a> |
+            <a href="http://www.anu.edu.au/">Australian National University</a> |
+            <a href="#">Contact Me</a>
+        </div>
+    </footer>
+
+@yield('pageSpecificJS')
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,68 @@
+<!--
+* File: index.blade.php
+* Owner: Alex Simangunsong
+* Purpose: Index page
+* Versions
+*   User    |       Date       |            Description              |
+*   Alex    |    06/09/2016    | Developed                           |
+!-->
+
+@extends('app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/search-box.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <div class="container-fluid text-center top-tiles">
+        <a href="#">
+            <img src="{{ asset('images/news-1.png') }}"
+                 class="img-thumbnail"
+                 width="10%"
+                 alt="News Icon">
+        </a>
+        <a href="#">
+            <img src="{{ asset('images/weather-1.png') }}"
+                 class="img-thumbnail"
+                 width="10%"
+                 alt="Weather Icon">
+        </a>
+        <a href="https://www.google.com.au/maps/search/hospital/@-35.317248,149.1443635,15z/data=!3m1!4b1">
+            <img src="{{ asset('images/hospital-2.ico') }}"
+                 class="img-thumbnail"
+                 width="10%"
+                 alt="Hospital Icon">
+        </a>
+    </div>
+
+    <div class="jumbotron text-center">
+        <h1>What are you looking for?</h1>
+        <br>
+        <br>
+
+        <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-push-3 col-md-6 col-lg-6">
+                <div class="input-group stylish-input-group">
+                    <input type="text" class="form-control"  placeholder="Search" >
+                    <span class="input-group-addon">
+                        <button type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <br><br>
+
+        <h2>OR</h2>
+
+        <br><br>
+
+        <div>
+            <img src="{{ asset('images/microphone-1.png') }}"
+                 width="5%"
+                 alt="Microphone Icon">
+        </div>
+    </div>
+@endsection
